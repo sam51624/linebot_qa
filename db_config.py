59d3a821c39720a92275b5734577e9e1f20f7064
sam51624 <sam51624@gmail.com>
2025-04-25 21:18:44 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # เชื่อมต่อ PostgreSQL
-DATABASE_URL = "postgresql://postgres:Sam219551624@34.132.113.178:5432/postgres"
+DATABASE_URL = "postgresql+psycopg2://postgres:mypassword@/postgres?host=/cloudsql/causal-relic-457214:g5:us-central1:kts-mini-db"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
