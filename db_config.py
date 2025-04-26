fa4e743093a12926369c4d078fdd6d411ee88bf1
sam51624 <sam51624@gmail.com>
2025-04-26 10:39:49 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,7 +1,8 @@
+# db_config.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# ใช้ Public IP แทน Unix socket
+# เชื่อมต่อ PostgreSQL ผ่าน Public IP
 DATABASE_URL = "postgresql+psycopg2://postgres:Sam219551624@34.132.113.178:5432/postgres"
 
 engine = create_engine(DATABASE_URL, echo=False)
